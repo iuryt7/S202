3ba547e657b41f14a5039967e60293531dc582d7
Iury Teixeira <113626147+iuryt7@users.noreply.github.com>
2024-09-02 13:20:50 -0300

    Add files via upload

---
+++ Teórico/Trabalho-1/main.py
@@ -0,0 +1,14 @@
+import threading
+
+from Thread import Sensores
+from database import Database
+#Criando uma instancia
+from database import Database
+
+db = Database(database="bancoiot", collection="sensores")
+sensores = Sensores(db)
+threads = []
+for i in range(1, 4):
+    t = threading.Thread(target=sensores.sensor, args=(f"Sensor{i}", 5))
+    t.start()
+    threads.append(t)
